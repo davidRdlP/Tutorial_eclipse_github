982ed7e5b5f7dc156eb8452712796ca35a94b65f
davidRdlP <darope19@gmail.com>
2021-10-25 12:09:29 +0200

    prueba2

--- java__/nose.java
+++ java__/nose.java
@@ -3,6 +3,6 @@ package java__;
 public class nose {
     public static void main(String[] args) {
         int numero = 5;
-        System.out.println(numero);
+        System.out.println(numero/2);
     }
 }
