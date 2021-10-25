bd557977f1925457796c687a7cd077ff1e17541c
davidRdlP <darope19@gmail.com>
2021-10-25 12:13:12 +0200

    prueba3

--- java__/nose.java
+++ java__/nose.java
@@ -4,5 +4,8 @@ public class nose {
     public static void main(String[] args) {
         int numero = 5;
         System.out.println(numero/2);
+        double decimal = 2.5;
+        double resultado= numero*decimal;
+        System.out.println(resultado);
     }
 }
